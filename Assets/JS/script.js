54b5347fbc7426da0fb06910a5c587a9c8e31f70
Jesse De Jong <jdoceau@gmail.com>
2021-06-30 18:08:40 +1000

    Operational API call function to return a random image meeting the user-search-criteria

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,14 +1,27 @@
 // DOM targeting for content injection
 var testbench = document.getElementById('contentINJ');
 var userQuery = document.getElementById('userQuery');
+var button = document.getElementById('goBtn');
 
 // Metropolitan Meseum API
 var metUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search";
-var metImg = "?primaryImage"
-var imgCheck = "?hasimages=true"
-var query = ''
+var metObjUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+var metImg = "?primaryImage";
+var imgCheck = "?hasimages=true";
 
-function newSearch (url, param1, query) {   // URL source, Additional paramater, User search query
+// Write to DOM
+function randomResult (objectIDs) {
+    var i = Math.floor(Math.random() * objectIDs.length);   // Pick a random item from the valid results array
+
+    // testbench.innerHTML = objectIDs[i];
+
+    url = metObjUrl + objectIDs[i];
+    imgReturn(url, metImg);
+}
+
+// Core search function
+function newSearch (url, param1, query) {   // Pass in variables
+
     fetch(url + param1 + query, {
         method: 'GET',
         // mode: 'no-cors',
@@ -17,9 +30,31 @@ function newSearch (url, param1, query) {   // URL source, Additional paramater,
         return response.json();
     })
     .then(function (data) {                 // Pull array of object ID's meeting criteria
-
-        testbench.innerHTML = data.objectIDs;
+        console.log(data, data.objectIDs);
+        // testbench.innerHTML = data.objectIDs;   // Write returned values to the DOM
+        randomResult(data.objectIDs);
     })
 }
 
-// newSearch(metUrl, imgCheck, '&q=' + query);   // Test use of metURL
+// Image Retrieval function
+function imgReturn (url) {   // Pass in variables
+
+    fetch(url, {
+        method: 'GET',
+        // mode: 'no-cors',
+    })
+    .then(function (response) {             // Take response and parse it with JSON
+        return response.json();
+    })
+    .then(function (data) {                 // Display primary image result
+        testbench.innerHTML += "<img src=" + data.primaryImage + "></img>"; 
+    })
+}
+
+// Eventlistener for button press
+button.addEventListener("click", function(event) {
+    event.preventDefault();                         // Anonymous function to prevent form reset
+
+    var query = userQuery.value;                    // Update user input variable
+    newSearch(metUrl, imgCheck, '&q=' + query);     // Perform a new serach with the chosen paramaters
+})
